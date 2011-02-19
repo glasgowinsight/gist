b44c44abe499ccd38fc796e77d57a6e8282199a7
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-02-19 20:40:26 +0000

    Synced with server

--- httpdocs/wp-content/themes/test/loop.php
+++ httpdocs/wp-content/themes/test/loop.php
@@ -126,7 +126,7 @@
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<?php the_post_thumbnail(); ?>
-            <div class="entry-summary">
+                        <div class="entry-summary">
 				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
 		</div><!-- #post-## -->
