8f19ad358848eb5d017793adb421a9a328bd662a
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-03-02 19:58:12 +0000

    Updated post widths

--- theme/home2.php
+++ theme/home2.php
@@ -95,13 +95,25 @@ Template Name: Home 2
 
 <div id="snippets">
 	<?php 
+		function custom_excerpt_length( $length ) {
+			return 20;
+		}
+		add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 		$posts = array();
 		$ids = array();
 		add_posts( 'snippet', 5, $posts, $ids );
 		foreach ($posts as $post):
-	    	setup_postdata($post); 
-			show_post_excerpt();
+	    	setup_postdata($post); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+			
+				<?php the_post_thumbnail(); ?>
+			    <div class="entry-summary">
+					<?php the_excerpt(); ?>
+				</div>
+			</div><?php 
 	    endforeach;
+	    remove_filter( 'excerpt_length', 'custom_excerpt_length' );
 	?>
 </div>
 
