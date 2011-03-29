681f2c1079f0f661c800444fa5ff02cd10172e6a
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-03-29 22:21:49 +0100

    Tidying up sidebar

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -8,59 +8,58 @@
  */
 ?>
 <div id="sidebar">
-
-		<div id="primary" role="complementary">
-			
-			<?php 
-				query_posts( array(
-					'category_name'=>'event',
-					'num_posts'=>5
-				));
-				if(have_posts()){
-					?><div id="events"><ul><?php 
-					while (have_posts()){
-						the_post(); 
-		            	?><li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li><?php 
-					}
-					?></ul></div><?php 
+	<div id="primary" role="complementary">
+		<?php 
+			query_posts( array(
+				'category_name'=>'event',
+				'num_posts'=>5
+			));
+			if(have_posts()){
+				?><div id="events" class="sidebarSection"><ul><?php 
+				while (have_posts()){
+					the_post(); 
+	            	?><li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li><?php 
 				}
-			?>
-			<div class="sidebarSection">
-			<h3> Find items about </h3>
-			
-			<ul> 
-				<li>Local Glasgow science </li> 
-				<li>Internationsl research </li> 
-				<li>interviews with scientists </li> 
-				<li>Current Events </li> 
-				<li>Opinions </li> 
+				?></ul></div><?php 
+			}
+		?>
+		
+		<?php 
+			$tags = get_tags();
+			if(count($tags)>0){?>
+				<div class="sidebarSection">
+					<h3> Find out about </h3> 
+					<ul> 
+						<?php 
+							foreach ($tags as $tag){
+								?><li><a href="<?php echo get_tag_link($tag->term_id)?>"><?php echo $tag->name ?></a></li><?php 
+							}
+						?>
+					</ul>
+				</div><?php 
+			}
+		?>
+		
+		<div class="sidebarSection">
+			<script>show_tweets();</script>
+		</div>		
+
+		<div class="sidebarSection">
+			<h3>Keep In Touch</h3>
+			<ul>
+				<li><a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/rss.png"/>Get the latest news</a></li>
+				<li><a href="mailto:glasgowinsight@gmail.com"><img src="<?php bloginfo('template_directory'); ?>/images/email.png"/>Tell us what's happening</a></li>
+				<li><a href="http://twitter.com/GlasgowGist"><img src="<?php bloginfo('template_directory'); ?>/images/twitter.png"/>Follow us on Twitter</a></li>
+				<li><a href="http://www.facebook.com/pages/The-GIST-Glasgow-Insight-into-Science-and-Technology/185836941455238"><img src="<?php bloginfo('template_directory'); ?>/images/facebook.jpg"/>Talk to us on Facebook</a></li>
 			</ul>
-			</div> <!-- first sidebarSection -->
-	
-	
-			<div class="sidebarSection">
-			<h3> Intererest? Find out about </h3> 
-			<ul> 
-				<li>Physics </li> 
-				<li>Engineering</li> 
-				<li>Biology</li> 
-				<li>Chemistry</li> 
+		</div>
+		
+		<div class="sidebarSection">
+			<h3>Around The Web</h3>
+			<ul>
+				<li><a href="http://www.bluesci.org/">Bluesci</a></li>
+				<li><a href="http://www.eusci.org/">EUSci</a></li>
 			</ul>
-			</div> <!-- second sidebarSection -->
-
-		</div><!-- #primary .widget-area -->
-
-<?php
-	// A second sidebar for widgets, just because.
-	if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
-
-		<div id="secondary" class="widget-area" role="complementary">
-			<ul class="xoxo">
-				<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-			</ul>
-		</div><!-- #secondary .widget-area -->
-
-<?php endif; ?>
-
-
-</div> <!-- #sidebar -->
+		</div>
+	</div>
+</div>
