236bad1815ebbf6d37d9f724704d638ad92c99b5
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-04-06 23:23:05 +0100

    Added styling for single post page

--- theme/single.php
+++ theme/single.php
@@ -14,14 +14,16 @@ get_header(); ?>
 		<div id="content" role="main">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<div id="post-<?php the_ID(); ?>"  <?php post_class('single-post'); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<h1 class="entry-title"><?php the_title(); ?> <?php do_action('addthis_widget', get_permalink(), the_title('', '', false), 'fb_tw_sc' ); ?></h1>
 
 					<div class="entry-content">
-						
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->					
-				</div><!-- #post-## -->
+					</div>
+					
+					<h2>Discussion <?php do_action('addthis_widget', get_permalink(), the_title('', '', false), 'small_toolbox' ); ?></h2>
+				
+					<?php comments_template( '', true ); ?>
+				</div>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 		<div id="sidebar">
