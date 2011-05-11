2d6cc127f9a13f2bad7756815ada9024a21f473c
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-05-11 21:48:12 +0100

    Rearranged sidebar

--- theme/sidebar-single.php
+++ theme/sidebar-single.php
@@ -15,19 +15,41 @@ get_header(); ?>
 		if($description){ ?>
 			<div class="sidebarSection">
 				<h3> About the Author </h3> 
-				<div class="authorImage"><?php userphoto_the_author_photo() ?></div>
+				<div class="authorImage">
+					<?php 
+						if(userphoto_exists(get_the_author_meta('ID')))
+	    					userphoto(get_the_author_meta('ID'));
+						else
+	    					echo get_avatar(get_the_author_meta('ID'), 75, '404');
+	    			?>
+				</div>
 				<div class="authorDescription"><?php echo $description; ?></div>
 			</div> <?php 
 		}
 
+		$ids = array();
+		$ids[] = get_the_ID();
+		
+		function format_article($article){
+			?><a href="<?php echo get_permalink($article->ID); ?>"><?php echo $article->post_title; ?></a><?php 
+		}
+		
+		$other = get_posts( array(
+			'numberposts'=>3,
+			'author_name'=>get_the_author(),
+			'post__not_in'=>$ids
+		));
+		foreach ($other as $p){
+			$ids[] = $p->ID;
+		}
+		
+		sidebar('author-other', 'Other articles by <a href="' . get_author_posts_url(get_the_author_meta('first_name')) . '">' . get_the_author() . '</a>', $other, 'format_article');
+		
 		$links = get_post_custom_values('external_link');
 		function format_link($links){
 			echo $link;
 		}
 		sidebar('find-out', 'Find out more', $links, 'format_link');
-		
-		$ids = array();
-		$ids[] = get_the_ID();
 		
 		$tags = get_the_tags();
 		$tag_ids = array();
@@ -40,21 +62,7 @@ get_header(); ?>
 			'post__not_in'=>$ids
 		));
 		
-		foreach ($similar as $p){
-			$ids[] = $p->ID;
-		}
-		function format_article($article){
-			?><a href="<?php echo get_permalink($article->ID); ?>"><?php echo $article->post_title; ?></a><?php 
-		}
 		sidebar('similar-articles', 'Similar articles', $similar, 'format_article');
-		
-		$other = get_posts( array(
-			'numberposts'=>3,
-			'author_name'=>get_the_author(),
-			'post__not_in'=>$ids
-		));
-	
-		sidebar('author-other', 'Other articles by <a href="' . get_author_posts_url(get_the_author_meta('first_name')) . '">' . get_the_author() . '</a>', $other, 'format_article');
 	?>	
 	<div class="sidebarSection">
 		<h3>Spread The Word</h3>
