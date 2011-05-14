6fdbc4433cd62c4e7360b70f967eda82252deb65
Steven Davies <Steven.Davies@cis.strath.ac.uk>
2011-05-14 22:47:39 +0100

    Further styling of ideas page

--- theme/category-idea.php
+++ theme/category-idea.php
@@ -30,8 +30,12 @@ get_header(); ?>
 	        array_multisort($votes, 'SORT_DESC', $posts);
 	        foreach($posts as $post){
 	        	setup_postdata($post); ?>
-	        	<h3><?php the_title(); DisplayVotes(the_ID()); ?></h3>
-	        	<p><?php the_content(); ?></p> <?php 
+	        	<div class="vote"><?php DisplayVotes(get_the_ID()); ?></div>
+	        	<div class="idea">
+		        	<h3><?php the_title();  ?></h3>
+		        	<p><?php the_content(); ?></p>
+	        	</div><?php 
+	        	  
 	        }
         
 		?>
