b63bfe94760d3a6a80875062e3f8b2ee7ad6ab74
Steven Davies <el_loserio@ntlworld.com>
2012-01-09 20:41:17 +0000

    Added generic archive page

---
+++ theme/archive.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage The GIST
+ */
+
+get_header(); ?>
+
+		<section id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<h2 class="page-title cap-right">
+						<?php if ( is_day() ) : ?>
+							<span><?php the_date(); ?></span>
+						<?php elseif ( is_month() ) : ?>
+							<span><?php the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ); ?></span>
+						<?php elseif ( is_year() ) : ?>
+							<span><?php the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ); ?></span>
+						<?php else : ?>
+							<?php _e( 'Archives', 'twentyeleven' ); ?>
+						<?php endif; ?>
+					</h2>
+				</header>
+
+								<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'extract' ); ?>
+
+				<?php endwhile; ?>
+
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_footer(); ?>
