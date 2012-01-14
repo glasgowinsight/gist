60e967fb4dd822088843e3cdc3edb66d20ce173a
Steven Davies <el_loserio@ntlworld.com>
2012-01-14 16:01:08 +0000

    Attempt to use nested loop to display related posts

--- theme/single.php
+++ theme/single.php
@@ -12,18 +12,23 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php 
+				<?php 
+					if ( have_posts() ) :
+						the_post();
 						$rel = $related->show(get_the_ID(), true); 
 					    foreach ($rel as $r) :
-        					setup_postdata($r);
-        					the_post();
-        					get_template_part( 'content', 'extract' );
-    					endforeach;
-    					wp_reset_postdata();		
-					?>
+	        				setup_postdata($r);
+	        				the_post();
+	        				get_template_part( 'content', 'extract' );
+	    				endforeach;
+	    				rewind_posts();
+	    			endif;		
+				?>
 					
+				
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
 					<?php get_template_part( 'content', 'single' ); ?>
 
 					<?php comments_template( '', true ); ?>
