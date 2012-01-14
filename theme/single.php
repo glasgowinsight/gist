f8931b80a11db1db98978b16fc92544e537a5221
Steven Davies <el_loserio@ntlworld.com>
2012-01-14 16:22:26 +0000

    Added divs of latest articles

--- theme/single.php
+++ theme/single.php
@@ -20,20 +20,41 @@ get_header(); ?>
 
 				<?php endwhile;?>
 				
-				<?php 
-					if ( have_posts() ) :
-						global $post;
-						the_post();
-						$rel = $related->show(get_the_ID(), true); 
-					    foreach ($rel as $post) :
-	        				setup_postdata($post);
-	        				get_template_part( 'content', 'extract' );
-	    				endforeach;
-	    				wp_reset_postdata();
-	    			endif;		
-				?>
+				<?php if ( have_posts() ) : ?>
+					<?php global $post; ?>
+					<?php the_post(); ?>
+					<?php $rel = $related->show(get_the_ID(), true); ?>
+					<?php if ( $rel ): ?>
+							<div id="related">
+								<h2>Related Articles</h2>  
+								<?php foreach ($rel as $post) : ?>
+				        			<?php setup_postdata($post); ?>
+				        			<?php get_template_part( 'content', 'extract' ); ?>
+				    			<?php endforeach; ?>
+				    			<?php wp_reset_postdata(); ?>
+				    		</div>
+	    			<?php endif; ?>
+	    		<?php endif; ?>		
 				
-
+				<?php query_posts('category_name=feature,snippet&posts_per_page=7'); ?>
+				<?php $i=0; ?>
+				<?php if ( have_posts() ) : ?>
+					<div id="latest">
+						<h2>Latest Articles</h2>  
+						<?php while ( $i<3 && have_posts() ) : the_post; ?>
+		        			<?php get_template_part( 'content', 'extract' ); ?>
+		    			<?php endwhile; ?>
+		    		</div>
+	    		<?php endif; ?>		
+				<?php if ( have_posts() ) : ?>
+					<div id="other">
+						<h2>Other Articles</h2>  
+						<?php while ( have_posts() ) : the_post; ?>
+		        			<?php get_template_part( 'content', 'extract' ); ?>
+		    			<?php endwhile; ?>
+		    		</div>
+	    		<?php endif; ?>		
+				
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
