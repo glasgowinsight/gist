8dc895d066af2642fc14ee7f28f87d47afd530e9
Steven Davies <el_loserio@ntlworld.com>
2012-01-14 16:48:33 +0000

    Reordered divs on article page

--- theme/content-single.php
+++ theme/content-single.php
@@ -15,21 +15,5 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div>
-
-	<footer class="entry-meta">
-		<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : ?>
-		<div id="author-info">
-			<div id="author-avatar">
-				<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-					<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 68 ) ); ?>
-				</a>
-			</div><!-- #author-avatar -->
-			<div id="author-description">
-				// <?php the_author_meta( 'description' ); ?>
-			</div>
-		</div>
-		<?php endif; ?>
-	</footer>
 </article>
--- theme/single.php
+++ theme/single.php
@@ -9,32 +9,39 @@
 
 get_header(); ?>
 
+<?php if ( have_posts() ): the_post();?>
+<?php $main_post<-get_postdata(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php $rel = $related->show($main_post->ID, true); ?>
+				<?php if ( $rel ): ?>
+						<div id="related">
+							<h2>Related Articles</h2>  
+							<?php global $post; ?>
+							<?php foreach ($rel as $post) : ?>
+			        			<?php setup_postdata($post); ?>
+			        			<?php get_template_part( 'content', 'extract' ); ?>
+			    			<?php endforeach; ?>
+			    		</div>
+    			<?php endif; ?>
 
-					<?php get_template_part( 'content', 'single' ); ?>
-
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile;?>
+				<?php $post = $main_post; setup_postdata($post); ?>
+				<?php get_template_part( 'content', 'single' ); ?>
 				
-				<?php if ( have_posts() ) : ?>
-					<?php global $post; ?>
-					<?php the_post(); ?>
-					<?php $rel = $related->show(get_the_ID(), true); ?>
-					<?php if ( $rel ): ?>
-							<div id="related">
-								<h2>Related Articles</h2>  
-								<?php foreach ($rel as $post) : ?>
-				        			<?php setup_postdata($post); ?>
-				        			<?php get_template_part( 'content', 'extract' ); ?>
-				    			<?php endforeach; ?>
-				    			<?php wp_reset_postdata(); ?>
-				    		</div>
-	    			<?php endif; ?>
-	    		<?php endif; ?>		
+				<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : ?>
+				<div id="author-info">
+					<h2>Author</h2>
+					<div id="author-avatar">
+						<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 68 ) ); ?>
+						</a>
+					</div><!-- #author-avatar -->
+					<div id="author-description">
+						// <?php the_author_meta( 'description' ); ?>
+					</div>
+				</div>
+				<?php endif; ?>
 				
 				<?php query_posts('category_name=feature,snippet&posts_per_page=7'); ?>
 				<?php $i=0; ?>
@@ -56,7 +63,11 @@ get_header(); ?>
 		    		</div>
 	    		<?php endif; ?>
 				
+				<?php $post = $main_post; setup_postdata($post); ?>
+				<?php comments_template( '', true ); ?>
+				
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
+<?php endif; ?>
 <?php get_footer(); ?>
