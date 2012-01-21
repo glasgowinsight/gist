91fd7484850451acd01c96f19d8494406fabba57
Steven Davies <el_loserio@ntlworld.com>
2012-01-21 19:25:31 +0000

    Shrank images for mobile browsers

--- theme/content-extract.php
+++ theme/content-extract.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('extract'); ?>>
 	<header class="entry-header">
 		<h3 class="entry-title"><?php the_title(); ?></h3>
 	</header>
@@ -20,4 +20,5 @@
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div>
+	<br style="clear:both"/>
 </article>
