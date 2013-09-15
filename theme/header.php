da5dafef1b7b242ebb03e8e30706aaa6ed2ba044
yuliarevina <lingualsponge@yahoo.co.uk>
2013-09-15 23:46:25 +0100

    New colours and logo
    Colours -new "darks" and "normals".
    Logo - new image, same background colour on each page.

--- theme/header.php
+++ theme/header.php
@@ -116,7 +116,7 @@
 	<div id="branding" role="banner">
 			<h2 id="logo">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> - <?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?>" rel="home">
-					<img width="212" height="99" src="<?php echo resource('images/logo.png'); ?>"/>
+					<img width="261" height="99" src="<?php echo resource('images/gist_logo_png.png'); ?>"/>
 				</a>
 			</h2>
 			
