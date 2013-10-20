8ca8802871214f9e6d1f8ce558943d911ae5c1e2
yuliarevina <lingualsponge@yahoo.co.uk>
2013-10-20 19:53:17 +0100

    Alignment of posts
    Image same width as text. 3 posts per section. 4 in latest.

--- theme/functions.php
+++ theme/functions.php
@@ -14,7 +14,7 @@ function gist_setup() {
 	// This theme uses post thumbnails
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'large_thumb', 456, 456, True );
-	add_image_size( 'medium_thumb', 221, 110, True );
+	add_image_size( 'medium_thumb', 300, 300, True );
 	add_image_size( 'small_thumb', 150, 110, True );
 
 	// Add default posts and comments RSS feed links to head
@@ -213,17 +213,18 @@ function the_short_title(){
 	if($title) echo $title; else the_title();
 }
 
-function get_extract( $classes = '', $thumb = 'small_thumb' ) {	?>
+function get_extract( $classes = '', $thumb = 'medium_thumb' ) {	?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class('extract ' . $classes); ?>>
-		<div class="entry-header">
-			<h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_short_title(); ?></a></h3>
-		</div>
+		
 		<div class="entry-thumbnail">
 			<div class="thumbnail-caption">
 				<a href="<?php the_permalink(); ?>" rel="bookmark">
 					<?php the_post_thumbnail( $thumb ); ?>
 				</a>
 			</div>
+		</div>
+		<div class="entry-header">
+			<h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_short_title(); ?></a></h3>
 		</div>
 		<div class="entry-content">
 			<?php the_excerpt(); ?>
